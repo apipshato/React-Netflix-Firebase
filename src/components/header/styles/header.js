2723ddc4a6ec2c94f82cea3e01e528ea07428518
apipshato <afifshatos@gmail.com>
2020-09-25 01:26:48 +0700

    add logo and fixing logo

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,17 +1,37 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Link as ReactRouterLink } from "react-router-dom";
 
+export const Background = styled.div``;
 
-export const Background= styled.div`
+export const Frame = styled.div``;
+export const Container = styled.div``;
 
+export const Logo = styled.img`
+height: 32px;
+width: 108px;
+margin-right: 40px;
+
+@media(min-height: 1449px){
+    height: 45px;
+    width: 167px
+}
 `;
 
-export const Frame= styled.div`
+export const ButtonLink = styled(ReactRouterLink)`
+  display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
 
-`;
-export const Container= styled.div`
-
-`;
-
-export const  Logo= styled.img`
-
+  &:hover{
+      background-color: #f40612;
+  }
 `;
--- src/containers/header.js
+++ src/containers/header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { Header } from "../components";
 import * as ROUTES  from '../constants/routes'
+import logo from '../logo.svg';
 
 export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="Netflix"/>
-        <Header.Button />
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo}/>
+        <Header.ButtonLink to={ROUTES.SIGNUP}>Sign Up </Header.ButtonLink>
       </Header.Frame>
       {children}
     </Header>
