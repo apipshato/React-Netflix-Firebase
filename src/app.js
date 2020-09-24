c32db8fd7d71cade47a3cbc13fb6c197a5abf4b8
apipshato <afifshatos@gmail.com>
2020-09-24 16:30:47 +0700

    add pages end fix route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import {FooterContainer} from './containers/footer';
-import {FaqsContainer} from './containers/faqs';
- 
+import  { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
+ import{Home, Browse , Signin, Signup} from './pages';
 
 export default function App() {
-  return<>
-   <JumbotronContainer/>
-   <FaqsContainer/ >
-   <FooterContainer/>
+  return(
+  <Router>
+       <Route exact path="/user">
+    <p>Hello Ass Hole</p>
+   </Route>
+    <Route  path={ROUTES.HOME}>
+    <Home />
+   </Route>
 
-  </>
+   </Router>
+  );
 }
