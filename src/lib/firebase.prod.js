7c1dd793056ca520e27ce48e93a623e37591cd27
apipshato <afifshatos@gmail.com>
2020-09-25 22:07:02 +0700

    add fire base in index

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,12 +1,21 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import {seedDatabase} from '../seed';
 
 
 // we need some how seed database
 
 // we need config here
-const config ={}
-const Firebase =Firebase.initializeApp(config);
-
-export {firebase}
+const config ={
+    apiKey: "AIzaSyD9d3SfBw1jk0exiE-LT1PbpsiKggn_iEM",
+    authDomain: "netflix-clone-d2d74.firebaseapp.com",
+    databaseURL: "https://netflix-clone-d2d74.firebaseio.com",
+    projectId: "netflix-clone-d2d74",
+    storageBucket: "netflix-clone-d2d74.appspot.com",
+    messagingSenderId: "327807642471",
+    appId: "1:327807642471:web:a0b1fd19d7f2f597229dc2"
+}
+const firebase =Firebase.initializeApp(config);
+seedDatabase(firebase);
+export {firebase};
