da46654f50b8ffa2f31c942c131ffbb1b84d3455
apipshato <afifshatos@gmail.com>
2020-09-29 22:15:17 +0700

    player and switch

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -36,7 +36,7 @@ export function BrowseContainer({ slides }) {
     } else {
       setSlideRows(slides[category]);
     }
-  }, [searchTerm]);
+  }, [category, searchTerm, slideRows, slides]);
 
   return profile.displayName ? (
     <>
