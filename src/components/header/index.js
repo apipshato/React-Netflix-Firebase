ee161c9902caa71456c534b0340a898c0723cf38
apipshato <afifshatos@gmail.com>
2020-10-13 23:17:25 +0700

    add group

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-import { Background, ButtonLink, Container, Logo,Feature,Text,FeatureCallOut, } from "./styles/header";
+import { Background, ButtonLink, Container, Logo,Feature,Text,FeatureCallOut,Link,Group } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children} </Background> : children;
@@ -12,11 +12,17 @@ Header.Feature = function HeaderFeatureCallOut({ childern, ...restProps }) {
 Header.Feature = function HeaderFeature({ childern, ...restProps }) {
   return <Feature {...restProps}>{childern}</Feature>;
 };
+Header.TextLink = function HeaderTextLink({ childern, ...restProps }) {
+  return <Link {...restProps}>{childern}</Link>;
+};
 Header.Text = function HeaderText({ childern, ...restProps }) {
   return <Text {...restProps}>{childern}</Text>;
 };
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
 };
 Header.ButtonLink = function HeaderButtonLinks({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
