23e121be833bd5b3dd08b4ad1823d31704e11cea
apipshato <afifshatos@gmail.com>
2020-10-14 00:00:54 +0700

    header fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-import { Background, ButtonLink, Container, Logo,Feature,Text,FeatureCallOut,Link,Group } from "./styles/header";
+import { Background, ButtonLink, Container, Logo,Feature,Text,FeatureCallOut,Link,Group, Picture,Profile, Dropdown } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children} </Background> : children;
@@ -9,12 +9,22 @@ export default function Header({ bg = true, children, ...restProps }) {
 Header.Feature = function HeaderFeatureCallOut({ childern, ...restProps }) {
   return <FeatureCallOut {...restProps}>{childern}</FeatureCallOut>;
 };
+Header.Profile = function HeaderProfile({childern, ...restProps}){
+return <Profile {...restProps}>{childern}</Profile>
+}
 Header.Feature = function HeaderFeature({ childern, ...restProps }) {
   return <Feature {...restProps}>{childern}</Feature>;
 };
 Header.TextLink = function HeaderTextLink({ childern, ...restProps }) {
   return <Link {...restProps}>{childern}</Link>;
 };
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
 Header.Text = function HeaderText({ childern, ...restProps }) {
   return <Text {...restProps}>{childern}</Text>;
 };
